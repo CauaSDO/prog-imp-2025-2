a44e42ce3ead9583303f31329c53013e036f5b01
Cauã Silva de Oliveira <97033771+CauaSDO@users.noreply.github.com>
2025-08-16 00:09:10 +0000

    revisão FOR

---
+++ ex01.revisao/Exemplo01.java
@@ -0,0 +1,16 @@
+public class Exemplo01 {
+    public static final int QTD_REPETIÇÕES = 20;
+    public static void main(String[] args) {
+
+        for(int i = QTD_REPETIÇÕES; i > 0; i--) {
+            System.out.println(i + " Olá, mundo!");
+        }
+        
+
+
+
+
+
+
+    }
+}
